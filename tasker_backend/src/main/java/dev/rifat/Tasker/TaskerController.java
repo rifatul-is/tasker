b0c163273e58090a0e24ec338884db7fc5581695
rifatulramim <58520916+rifatul-is@users.noreply.github.com>
2023-11-14 23:02:32 +0600

    get all task fetch request added
    solved cros error that backend throws

--- tasker_backend/src/main/java/dev/rifat/Tasker/TaskerController.java
+++ tasker_backend/src/main/java/dev/rifat/Tasker/TaskerController.java
@@ -11,6 +11,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/task")
+@CrossOrigin
 public class TaskerController {
     @Autowired
     private TaskService taskService;
--- tasker_frontend/script.js
+++ tasker_frontend/script.js
@@ -1,3 +1,5 @@
+const url = "http://localhost:8080/api/task";
+
 let addNewTaskCatagoryName = document.getElementById('addNewTaskCatagoryName');
 let addNewTaskCatagoryBtn = document.getElementById('addNewTaskCatagoryBtn');
 let taskCatagoryList = document.getElementById('taskCatagoryList');
@@ -6,10 +8,10 @@ let taskCatagoryListUl = document.getElementById('taskCatagoryListUl');
 let createNewTaskBtn = document.getElementById('createNewTaskBtn')
 
 let taskViewer = document.getElementById('taskViewer');
-let taskBox = document.getElementById('taskBox');
-let taskName = document.getElementById('taskName');
-let taskInfo = document.getElementById('taskInfo');
-let subTaskList = document.getElementById('subTaskList')
+// let taskBox = document.getElementById('taskBox');
+// let taskName = document.getElementById('taskName');
+// let taskInfo = document.getElementById('taskInfo');
+// let subTaskList = document.getElementById('subTaskList')
 
 let overlay = document.getElementById('overlay');
 let taskCreatorPopup = document.getElementById('taskCreatorPopup');
@@ -27,13 +29,61 @@ addNewTaskCatagoryBtn.addEventListener('click', function(){ addNewTaskCatagory()
 createNewTaskBtn.addEventListener('click', function(){ addNewTaskPopup() });
 closeCreateNewTaskPopup.addEventListener('click', function(){ closeNewTaskPopup() });
 
+var getRequest = {
+    method: 'GET'
+  };
+  
+
+function getAllTask() {
+
+
+
+
+    fetch(url, getRequest)
+    .then(response => response.json())
+    .then(data => {
+        data.forEach(task => {
+            let taskBox = document.createElement('div');
+            taskBox.classList.add('task-box');
+
+            let taskName = document.createElement('h3');
+            taskName.classList.add('task-name');
+            taskName.innerHTML = task.taskTitle;
+
+            let taskInfo = document.createElement('p');
+            taskInfo.classList.add('task-info');
+            taskInfo.innerHTML = task.taskInfo;
+
+            let subTaskUl = document.createElement('ul');
+
+            taskViewer.appendChild(taskBox);
+            taskBox.appendChild(taskName);
+            taskBox.appendChild(taskInfo);
+            taskBox.appendChild(subTaskUl);
+
+            console.log(task.taskTitle);
+
+            task.taskPreReq.forEach(subTask => {
+                let subTaskList = document.createElement('li');
+                subTaskList.innerHTML = subTask;
+                subTaskUl.appendChild(subTaskList);
+
+                console.log(subTask);
+            });
+
+
+        });
+        console.log(data);
+    })
+    .catch(error => console.log('error', error));
+}
+
+
 function addNewTaskCatagory () {
     if (addNewTaskCatagoryName.value.length >= 4) {
         var newTaskCatagory = document.createElement('li')
         var newTaskCatagoryItem = document.createElement('a')
                 
-        //newTaskCatagory.classList.add('task-catagoty-list-item', 'task-catagoty-list-item:hover');
-
         taskCatagoryListUl.appendChild(newTaskCatagory);
         newTaskCatagory.appendChild(newTaskCatagoryItem);
         newTaskCatagoryItem.innerText = addNewTaskCatagoryName.value;
@@ -49,4 +99,6 @@ function closeNewTaskPopup () {
     overlay.style.display = "none";
     taskCreatorPopup.style.display = "none";
 }
+
+getAllTask();
 
